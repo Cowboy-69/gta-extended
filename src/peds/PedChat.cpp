d9d3c7f26a9bc9b86596c50801c7ad28ab099ca1
Cowboy69 <scientistman999@gmail.com>
2024-05-11 22:16:28 +0500

    Peds don't say lines when they're dead

--- src/peds/PedChat.cpp
+++ src/peds/PedChat.cpp
@@ -103,6 +103,11 @@ CPed::ServiceTalking(void)
 void
 CPed::Say(uint16 audio)
 {
+#ifdef IMPROVED_TECH_PART // Peds don't say lines when they're dead
+	if (Dead())
+		return;
+#endif
+
 	if (3.0f + TheCamera.GetPosition().z < GetPosition().z)
 		return;
 
