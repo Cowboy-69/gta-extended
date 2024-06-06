10aaaa654eec79b9b4d84ca9da0433698676e5ec
Cowboy69 <scientistman999@gmail.com>
2024-06-06 17:53:09 +0500

    Player's clothes are not reset after death or arrest

--- src/control/GameLogic.cpp
+++ src/control/GameLogic.cpp
@@ -89,10 +89,14 @@ CGameLogic::SortOutStreamingAndMemory(const CVector &pos)
 	CStreaming::DeleteRwObjectsAfterDeath(pos);
 	CStreaming::RemoveUnusedModelsInLoadedList();
 	CGame::DrasticTidyUpMemory(true);
+#ifndef IMPROVED_TECH_PART // Player's clothes are not reset after death or arrest
 	CWorld::Players[CWorld::PlayerInFocus].m_pPed->Undress("player");
+#endif
 	CStreaming::LoadSceneCollision(pos);
 	CStreaming::LoadScene(pos);
+#ifndef IMPROVED_TECH_PART // Player's clothes are not reset after death or arrest
 	CWorld::Players[CWorld::PlayerInFocus].m_pPed->Dress();
+#endif
 	CTimer::Update();
 }
 
